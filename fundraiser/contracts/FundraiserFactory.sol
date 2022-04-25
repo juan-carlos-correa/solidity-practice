1f9972842dfc141da8be624b1463d520a25108e1
juan-carlos-correa <juan.correa.herrera@gmail.com>
2022-04-25 11:02:20 -0500

    added fundraisers getter

--- fundraiser/contracts/FundraiserFactory.sol
+++ fundraiser/contracts/FundraiserFactory.sol
@@ -4,6 +4,8 @@ pragma solidity >=0.4.22 <0.9.0;
 import './Fundraiser.sol';
 
 contract FundraiserFactory {
+  uint256 constant maxLimit = 20;
+
   Fundraiser[] private _fundraisers;
 
   event FundraiserCreated(Fundraiser indexed fundraiser, address indexed owner);
@@ -31,5 +33,21 @@ contract FundraiserFactory {
     _fundraisers.push(fundraiser);
 
     emit FundraiserCreated(fundraiser, msg.sender);
+  }
+
+  function fundraisers(
+    uint256 limit, 
+    uint256 offset
+    ) public view returns (Fundraiser[] memory coll) {
+      uint256 size = fundraisersCount() < limit ? fundraisersCount() : limit;
+      size = size < maxLimit ? size : maxLimit;
+
+      coll = new Fundraiser[](size);
+
+      for (uint256 i = 0; i < size; i++) {
+        coll[i] = _fundraisers[offset + i];
+      }
+
+      return coll;
   }
 }
--- fundraiser/test/fundraiser_factory_test.js
+++ fundraiser/test/fundraiser_factory_test.js
@@ -1,4 +1,5 @@
 const FundraiserFactoryContract = artifacts.require("FundraiserFactory");
+const FundraiserContract = artifacts.require("Fundraiser");
 
 contract("FundraiserFactory: deployment", () => {
   it("has been deployed", async () => {
@@ -53,5 +54,86 @@ contract("FundraiserFactory: createFundraiser", (accounts) => {
     const actualEvent = tx.logs[0].event;
 
     assert.equal(actualEvent, expectedEvent, "events should match");
+  });
+});
+
+contract("FundraiserFactory: fundraisers", (accounts) => {
+  async function createFundraiserFactory(fundraiserCount, accounts) {
+    const factory = await FundraiserFactoryContract.new();
+    await addFundraisers(factory, fundraiserCount, accounts);
+    return factory;
+  }
+
+  async function addFundraisers(factory, count, accounts) {
+    const name = "Beneficiary";
+    const lowerCaseName = name.toLowerCase();
+    const beneficiary = accounts[1];
+
+    for (let i = 0; i < count; i++) {
+      await factory.createFundraiser(
+        `${name} ${i}`,
+        `${lowerCaseName}${i}.com`,
+        `${lowerCaseName}${i}.png`,
+        `Description for ${lowerCaseName} ${i}`,
+        beneficiary
+      );
+    }
+  }
+
+  describe("when fundraisers collection is empty", () => {
+    it("returns an empty collection", async () => {
+      const factory = await createFundraiserFactory(0, accounts);
+      const fundraisers = await factory.fundraisers(10, 0);
+
+      assert.equal(fundraisers.length, 0, "collection should be empty");
+    });
+  });
+
+  describe("varying limits", async () => {
+    let factory = null;
+
+    beforeEach(async () => {
+      factory = await createFundraiserFactory(30, accounts);
+    });
+
+    it("returns 10 results when limit requested is 10", async () => {
+      const fundraisers = await factory.fundraisers(10, 0);
+
+      assert.equal(fundraisers.length, 10, "results size should be 10");
+    });
+
+    it("returns 20 results when limit requested is 20", async () => {
+      const fundraisers = await factory.fundraisers(20, 0);
+
+      assert.equal(fundraisers.length, 20, "results size should be 20");
+    });
+
+    it("returns 20 results when limit requested is 30", async () => {
+      const fundraisers = await factory.fundraisers(30, 0);
+
+      assert.equal(fundraisers.length, 20, "results size should be 20");
+    });
+  });
+
+  describe("varying offset", () => {
+    let factory = null;
+
+    beforeEach(async () => {
+      factory = await createFundraiserFactory(10, accounts);
+    });
+
+    it("contains the fundraiser with the appropriate offset when offset is 0", async () => {
+      const fundraisers = await factory.fundraisers(1, 0);
+      const fundraiser = await FundraiserContract.at(fundraisers[0]);
+      const name = await fundraiser.name();
+      assert.ok(await name.includes(0), `${name} did not include the offset`);
+    });
+
+    it("contains the fundraiser with the appropriate offset when the offset is 7", async () => {
+      const fundraisers = await factory.fundraisers(1, 7);
+      const fundraiser = await FundraiserContract.at(fundraisers[0]);
+      const name = await fundraiser.name();
+      assert.ok(await name.includes(7), `${name} did not include the offset`);
+    });
   });
 });
