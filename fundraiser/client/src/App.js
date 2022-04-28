51e0da3000cede9337c83557695ae89f461926a5
juan-carlos-correa <juan.correa.herrera@gmail.com>
2022-04-27 20:46:24 -0500

    eslint and prettier config

--- fundraiser/client/src/App.js
+++ fundraiser/client/src/App.js
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from "react";
-import FundraiserFactory from "./contracts/FundraiserFactory.json";
-import getWeb3 from "./getWeb3";
+import React, {useState, useEffect} from 'react'
+import FundraiserFactory from './contracts/FundraiserFactory.json'
+import getWeb3 from './getWeb3'
 
 export const App = () => {
   const [web3State, setWeb3State] = useState({
@@ -8,24 +8,24 @@ export const App = () => {
     web3: null,
     accounts: null,
     contract: null,
-  });
+  })
 
   useEffect(() => {
     const load = async () => {
       try {
         // Get network provider and web3 instance.
-        const web3 = await getWeb3();
+        const web3 = await getWeb3()
 
         // Use web3 to get the user's accounts.
-        const accounts = await web3.eth.getAccounts();
+        const accounts = await web3.eth.getAccounts()
 
         // Get the contract instance.
-        const networkId = await web3.eth.net.getId();
-        const deployedNetwork = FundraiserFactory.networks[networkId];
+        const networkId = await web3.eth.net.getId()
+        const deployedNetwork = FundraiserFactory.networks[networkId]
         const instance = new web3.eth.Contract(
           FundraiserFactory.abi,
-          deployedNetwork && deployedNetwork.address
-        );
+          deployedNetwork && deployedNetwork.address,
+        )
 
         // Set web3, accounts, and contract to the state, and then proceed with an
         // example of interacting with the contract's methods.
@@ -34,24 +34,24 @@ export const App = () => {
           web3,
           accounts,
           contract: instance,
-        });
+        })
       } catch (error) {
         // Catch any errors for any of the above operations.
         alert(
-          `Failed to load web3, accounts, or contract. Check console for details.`
-        );
-        console.error(error);
+          `Failed to load web3, accounts, or contract. Check console for details.`,
+        )
+        console.error(error)
       }
-    };
+    }
 
-    load().catch((e) => alert(e.message));
-  }, []);
+    load().catch(e => alert(e.message))
+  }, [])
 
   if (!web3State.web3) {
-    return <div>Loading Web3, accounts, and contract...</div>;
+    return <div>Loading Web3, accounts, and contract...</div>
   }
 
-  return <h1>Good to Go!</h1>;
-};
+  return <h1>Good to Go!</h1>
+}
 
-export default App;
+export default App
