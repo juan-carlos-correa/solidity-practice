4fb0480673b74a2ac2fc6f32d8101c87a7a69778
juan-carlos-correa <juan.correa.herrera@gmail.com>
2022-04-29 18:34:43 -0500

    create new fundraiser ui completed

--- fundraiser/client/src/pages/add-fundraiser/add-fundraiser.js
+++ fundraiser/client/src/pages/add-fundraiser/add-fundraiser.js
@@ -1,3 +1,4 @@
+import React from 'react'
 import Box from '@mui/material/Box'
 import TextField from '@mui/material/TextField'
 import Grid from '@mui/material/Grid'
@@ -5,7 +6,33 @@ import Button from '@mui/material/Button'
 import Container from '@mui/material/Container'
 import Typography from '@mui/material/Typography'
 
+import {Contract} from '../../providers'
+
 export const AddFundraiser = () => {
+  const web3 = React.useContext(Contract)
+
+  const handleSubmit = async e => {
+    e.preventDefault()
+
+    const {name, websiteUrl, imageUrl, description, beneficiary} =
+      e.target.elements
+
+    try {
+      await web3.contract.methods
+        .createFundraiser(
+          name.value,
+          websiteUrl.value,
+          imageUrl.value,
+          description.value,
+          beneficiary.value,
+        )
+        .send({from: web3.accounts[0]})
+      alert('created!')
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
     <Container maxWidth="md">
       <Box sx={{mt: 3}}>
@@ -13,7 +40,12 @@ export const AddFundraiser = () => {
           Add a new Fundraiser
         </Typography>
       </Box>
-      <Box component="form" noValidate sx={{mt: 3, mb: 3}}>
+      <Box
+        component="form"
+        noValidate
+        sx={{mt: 3, mb: 3}}
+        onSubmit={handleSubmit}
+      >
         <Grid container spacing={2}>
           <Grid item xs={12} sm={6}>
             <TextField name="name" fullWidth label="Name" />
@@ -28,7 +60,11 @@ export const AddFundraiser = () => {
             <TextField fullWidth name="description" label="Description" />
           </Grid>
           <Grid item xs={12}>
-            <TextField fullWidth name="custodian" label="Custodian address" />
+            <TextField
+              fullWidth
+              name="beneficiary"
+              label="Beneficiary address"
+            />
           </Grid>
         </Grid>
         <Box sx={{display: 'flex', justifyContent: 'flex-end'}}>
